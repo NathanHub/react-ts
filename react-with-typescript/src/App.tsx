8594221acc4630dc3f2789b9291129b2d747b474
Nathan <nathannba2424@gmail.com>
2022-05-07 17:21:00 -0400

    Add new features

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import List from "./components/List";
+//import List from "./components/List"; // Este import me da error
 
 
 function App() {
