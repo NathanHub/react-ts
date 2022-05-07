c96f61051cc73c90130f3baf05dce9d07f819f96
Nathan <nathannba2424@gmail.com>
2022-05-07 17:19:14 -0400

    Add new features

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List";
+
 
 function App() {
+
+  interface IState {
+    people: {
+      name: string, 
+      url:string, 
+      age:number,
+      note?: string
+    }[]
+  }
+
+  //const [number, setNumber] = useState(0);
+  //const [number, setNumber] = useState<number>(); // infer the type
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: "Lebron James",
+  //     url: "",
+  //     age: 36,
+  //     notes: "A legend at basketball"
+  //   },
+  //   {
+  //     name: "Joel Embiid",
+  //     url: "",
+  //     age: 26
+  //   }
+  // ]);
+
+  const [people, setPeople] = useState<IState['people']>(); // use of the interface
+  // const [people, setPeople] = useState<{name: string, url:string, age:number}[]>();
+
+  // people.map(person => {
+  //   person.name = ""
+  // })
+
+  // const changeNumber = () => {
+  //   setNumber(10);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>😎 People invited to the party 🎊</h1>
+     
     </div>
   );
 }
